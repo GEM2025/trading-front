c1dfa829cc92bcbb1aa9d7462a8302eb0a47707f
develHector <devel.hector@hotmail.com>
2023-04-16 22:39:06 -0600

    Fist ngx-datatable implementation on opportunities sub-component

--- src/app/components/opportunity/opportunity.component.ts
+++ src/app/components/opportunity/opportunity.component.ts
@@ -2,11 +2,12 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { CookieService } from 'ngx-cookie-service';
-import { Observable, Subject } from 'rxjs';
-import { Opportunity } from 'src/app/models/opportunity';
+import { BehaviorSubject, Observable, Subject, combineLatest } from 'rxjs';
+import { Opportunity, Step } from 'src/app/models/opportunity';
 import { SocketWebService } from 'src/app/services/socket-web.service';
 import { OpportunityModalComponent } from './opportunity.modal/opportunity.modal.component';
 import { OpportunityModalDataService } from './opportunity.modal.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-opportunity',
@@ -20,13 +21,25 @@ export class OpportunityComponent implements OnInit {
   private _opportunities$ = new Subject<Opportunity[]>();
   opportunities$: Observable<Opportunity[]> = this._opportunities$.asObservable();
 
+  sortType: any = 'multi';
+
+  private filterA$ = new BehaviorSubject<string>('');
+  private filterB$ = new BehaviorSubject<string>('');
+  private filterC$ = new BehaviorSubject<string>('');
+
+  filteredOpportunities$: Observable<Opportunity[]>;
+
+  // private stringify = function(s: Step | undefined):string {
+  //   return s ? `${s.exchange} ${s.side} ${s.name}` : '' ;
+  // };
+
   constructor(
     private router: ActivatedRoute,
     private cookieService: CookieService,
     private socketWebService: SocketWebService,
     private modalService: NgbModal,
     private opportunityModalDataService: OpportunityModalDataService
-    ) {
+  ) {
     this.side = "Loading...";
 
     // buy BTC/USD ----- buy USD/MXN  ------ buy MXN/BTC
@@ -38,6 +51,21 @@ export class OpportunityComponent implements OnInit {
       { id: 2, a: { side: "buy", name: "ABC/XYZ", exchange: "Kraken" }, b: { side: "buy", name: "XYZ/ABC", exchange: "Gemini" }, volume: 0.05, trade: 250 },
       { id: 3, a: { side: "buy", name: "ABC/XYZ", exchange: "Coinbase" }, b: { side: "sell", name: "ABC/XYZ", exchange: "HitBTC" }, volume: 0.05, trade: 200 },
     ];
+
+    const stringify = (s: Step | undefined): string => {
+      return s ? `${s.exchange} ${s.side} ${s.name}` : '';
+    };
+
+
+    this.filteredOpportunities$ = combineLatest([this.opportunities$, this.filterA$, this.filterB$, this.filterC$]).pipe(
+      map(([opportunities, filterA, filterB, filterC]) =>
+        opportunities.filter(o =>
+          stringify( o.a ).toLowerCase().includes(filterA.toLowerCase()) &&
+          stringify( o.b ).toLowerCase().includes(filterB.toLowerCase()) &&
+          stringify( o.c ).toLowerCase().includes(filterC.toLowerCase())
+      )
+      )
+    );
   }
 
   ngOnInit(): void {
@@ -48,6 +76,25 @@ export class OpportunityComponent implements OnInit {
     // we are using a service "OpportunityModalDataService" as a data bridge between the opportunity class and the modal
     this.opportunityModalDataService.data = `${this.side} Loading...`;
     const modalRef = this.modalService.open(OpportunityModalComponent);
+  }
+
+  getInputValue(event: Event): string {
+    return (event.target as HTMLInputElement).value;
+  }
+
+  onFilterA(search: string): void {
+    console.log(`filter ${search}`);
+    this.filterA$.next(search);
+  }
+
+  onFilterB(search: string): void {
+    console.log(`filter ${search}`);
+    this.filterB$.next(search);
+  }
+
+  onFilterC(search: string): void {
+    console.log(`filter ${search}`);
+    this.filterC$.next(search);
   }
 
 
--- src/app/models/opportunity.ts
+++ src/app/models/opportunity.ts
@@ -13,3 +13,6 @@ export interface Opportunity {
   trade: number;
 }
 
+
+
+
