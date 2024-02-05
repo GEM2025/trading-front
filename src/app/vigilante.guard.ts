cfb41e24f545c9d71f3c4382497a6b02809744e7
develHector <devel.hector@hotmail.com>
2024-02-05 15:52:02 -0600

    pre-16

--- src/app/vigilante.guard.ts
+++ src/app/vigilante.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, CanDeactivate, CanLoad, CanMatch, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class VigilanteGuard implements CanActivate, CanActivateChild, CanDeactivate<unknown>, CanLoad, CanMatch {
+export class VigilanteGuard  {
 
   constructor(private cookieService: CookieService, private router: Router) {
   }
